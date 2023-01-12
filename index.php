e1b57d744e7377df6a693bf3b0c6d03a881a0424
Damien <damien.deboudt@sfr.fr>
2023-01-12 18:38:51 +0100

    en cours

--- index.php
+++ index.php
@@ -1,1 +1,15 @@
 <?php
+echo 'aujourd\'hui il fait -1000 degrés';
+
+echo"<br><br>";
+
+$text = "je suis le noob";
+echo strtoupper($text) . "<br>";
+echo ucfirst($text) . "<br>";
+echo ucwords($text) . "<br>";
+
+echo"<br><br>";
+
+$uppertext = "JE SUIS LE NOUVEAU NOOB";
+
+echo strtolower($uppertext) . "<br>";
